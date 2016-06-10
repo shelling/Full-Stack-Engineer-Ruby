f59ec8cee5e28c8607cbcfae85d94b6dc64413f4
shelling <navyblueshellingford@gmail.com>
2016-06-10 22:35:33 +0800

    Refine Comic model

--- app/models/comic.rb
+++ app/models/comic.rb
@@ -1,11 +1,13 @@
 class Comic < ActiveRecord::Base
+  serialize :thumbnail, Hash
+
   def self.retrieve(options = {})
     Rails.application.config.marvel.comics(options).map do |comic|
       find_by(id: comic.id) || create(
         id:         comic.id,
         title:      comic.title,
-        modified:   DateTime.parse(comic.modified),
-        thumbnail:  "#{comic.thumbnail.path}.#{comic.thumbnail.extension}",
+        modified:   comic.modified,
+        thumbnail:  comic.thumbnail.as_json,
       )
     end
   end
